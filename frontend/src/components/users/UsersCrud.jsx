06aeb588b2486abb0711d216095b78b56df34f13
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-01-25 16:09:18 -0300

    editar/excluir implementado

--- frontend/src/components/users/UsersCrud.jsx
+++ frontend/src/components/users/UsersCrud.jsx
@@ -14,8 +14,14 @@ const initialState = {
 	list: [],
 };
 
-export default class UsersCrud extends Component {
+export default class UserCrud extends Component {
 	state = { ...initialState };
+
+	componentWillMount() {
+		axios(baseURL).then((resp) => {
+			this.setState({ list: resp.data });
+		});
+	}
 
 	clear() {
 		this.setState({ user: initialState.user });
@@ -27,16 +33,16 @@ export default class UsersCrud extends Component {
 		const url = user.id ? `${baseURL}/${user.id}` : baseURL;
 		axios[method](url, user).then((resp) => {
 			const list = this.getUpdatedList(resp.data);
-			this.setState = {
+			this.setState({
 				user: initialState.user,
-				list: list,
-			};
+				list,
+			});
 		});
 	}
 
-	getUpdatedList(user) {
+	getUpdatedList(user, add = true) {
 		const list = this.state.list.filter((u) => u.id !== user.id);
-		list.unshift(user);
+		if (add) list.unshift(user);
 		return list;
 	}
 
@@ -52,7 +58,7 @@ export default class UsersCrud extends Component {
 				<div className='row'>
 					<div className='col-12 col md-6'>
 						<div className='form-group'>
-							<label>Nome: </label>
+							<label>Nome:</label>
 							<input
 								type='text'
 								className='form-control'
@@ -66,7 +72,7 @@ export default class UsersCrud extends Component {
 
 					<div className='col-12 col md-6'>
 						<div className='form-group'>
-							<label>Cidade: </label>
+							<label>Cidade:</label>
 							<input
 								type='text'
 								className='form-control'
@@ -79,12 +85,12 @@ export default class UsersCrud extends Component {
 					</div>
 				</div>
 
-				<hr />
 				<div className='row'>
 					<div className='col-12 d-flex justify-content-end'>
-						<button className='btn btn-primary' onClick={(e) => this.save(e)}>
+						<button className='btn btn-success' onClick={(e) => this.save(e)}>
 							Salvar
 						</button>
+
 						<button
 							className='btn btn-secondary ml-2'
 							onClick={(e) => this.clear(e)}
@@ -96,7 +102,65 @@ export default class UsersCrud extends Component {
 			</div>
 		);
 	}
+
+	load(user) {
+		this.setState({ user });
+	}
+
+	remove(user) {
+		axios.delete(`${baseURL}/${user.id}`).then((resp) => {
+			const list = this.getUpdatedList(user, false);
+			this.setState({ list });
+		});
+	}
+
+	renderTable() {
+		return (
+			<table className='table mt-4'>
+				<thead>
+					<th>Id</th> <th>Nome</th>
+					<th>Cidade</th>
+					<th>Editar/Remover</th>
+				</thead>
+				<tbody>{this.renderRows()}</tbody>
+			</table>
+		);
+	}
+
+	renderRows() {
+		return this.state.list.map((user) => {
+			return (
+				<tr key={user.id}>
+					<td>{user.id}</td>
+					<td>{user.name}</td>
+					<td>{user.city}</td>
+					<td>
+						<button
+							className='btn btn-outline-primary'
+							onClick={() => this.load(user)}
+						>
+							<i className='fa fa-pencil'></i>
+						</button>
+						<button
+							className='btn btn-outline-danger ml-2'
+							onClick={() => this.remove(user)}
+						>
+							<i className='fa fa-trash'></i>
+						</button>
+					</td>
+				</tr>
+			);
+		});
+	}
+
 	render() {
-		return <Main {...headerProps}>{this.renderForm()}</Main>;
+		return (
+			<Main {...headerProps}>
+				{this.renderForm()}
+				<hr className='mt-4' />
+				<h2 className='text-muted mt-3'>Cadastrados</h2>
+				{this.renderTable()}
+			</Main>
+		);
 	}
 }
